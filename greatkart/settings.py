17c8d8bcbb974686a1071d3b673b977c8b8198e4
Enis Öksüz <wildclawzhd@hotmail.com>
2022-10-20 12:28:10 +0300

    added email verification and dashboard

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -142,3 +142,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.INFO: 'danger',
 }
+
+
+
+# SMTP configuration
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='asistanglobalcompany@gmail.com'
+EMAIL_HOST_PASSWORD= 'qgccdxrqgitojxdk'
+EMAIL_USE_TLS=True
